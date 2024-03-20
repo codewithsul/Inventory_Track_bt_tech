c8280502e87a45ee0247e89edb41615fbc1d4361
DannySamy3 <suleiman4a@gmail.com>
2024-03-20 17:00:44 +0300

    changes to UI

--- src/App.js
+++ src/App.js
@@ -59,50 +59,15 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
+        <View className="NavBar">
+           <View className="LeftButtonBlock">
+              <h2>Invetory</h2>
+              <label id="dashboard">DashBoard</label>
+           </View>
+           <View className="ButtonBlock">
+             <input type="button" value="Add Inventory" name="add_inventory" id="inventory_button" />
+           </View>
+        </View>
     </View>
   );
 };
